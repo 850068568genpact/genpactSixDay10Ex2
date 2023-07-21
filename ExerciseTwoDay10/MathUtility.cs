6a9886df83801b3239dc6fa4f88a16352e834d3b
850068568 genpact <850068568@genpact.com>
2023-07-21 17:19:08 +0530

    Initial Commit

---
+++ ExerciseTwoDay10/MathUtility.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseTwoDayTen
+{
+    public class MathUtility
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by Zero.");
+            }
+            return a / b;
+        }
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number == 2)
+                return true;
+            if (number % 2 == 0)
+                return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ExerciseTwoDay10/MathUtilityTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExerciseTwoDayTen;
+
+namespace ExerciseTwoDay10
+{
+    [TestFixture]
+    internal class MathUtilityTests
+    {
+        [Test]
+        public void AddTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            int result = mathUtility.Add(5, 10);
+            Assert.AreEqual(15, result);
+        }
+        [Test]
+        public void SubTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            int result = mathUtility.Subtract(10, 5);
+            Assert.AreEqual(5, result);
+        }
+        [Test]
+        public void MultiTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            int result = mathUtility.Multiply(10, 5);
+            Assert.AreEqual(50, result);
+        }
+        [Test]
+        public void DivTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            int result = mathUtility.Divide(10, 5);
+            Assert.AreEqual(2, result);
+        }
+        [Test]
+        public void IsPrimeTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            Assert.IsTrue(mathUtility.IsPrime(7));
+        }
+        [Test]
+        public void DivideByZeroTest()
+        {
+            MathUtility mathUtility = new MathUtility();
+            Assert.Throws<DivideByZeroException>(() => mathUtility.Divide(10, 0));
+        }
+    }
+}
